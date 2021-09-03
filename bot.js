fd5e864b2d07f80e84513d15a33d0e1cf6a249ad
lordaksik <48729120+lordaksik@users.noreply.github.com>
2021-09-03 13:47:52 +0300

    first5

--- bot.js
+++ bot.js
@@ -2,10 +2,9 @@
 const { Telegraf } = require('telegraf')
 const fetch = require('node-fetch');
 const bot = new Telegraf(process.env.BOT_TOKEN||8080)
-bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name}`))
-bot.help((ctx) => ctx.reply('Напиши /бот'))
-
-
+bot.start((ctx) => {ctx.reply(`Привет ${ctx.message.from.first_name}`)
+console.log(ctx.message)})
+bot.help((ctx) => ctx.reply('Напиши /bot'))
 bot.hears('/bot', async (ctx) => {
     async function request2(){
 
@@ -17,23 +16,60 @@ bot.hears('/bot', async (ctx) => {
         colors =data.items.results[i].results[0].color
        arr.push(colors);
     }
-        //if((data.items.results[i].results[0].color==="grey")&& (data.items.results[i].results[0].color===data.items.results[i+1].results[0].color)) {
-        //	 alert("раздача №"+i);}
-        if(arr[0]==="red")
-        { 
-            ctx.reply('Ставим:  ⬛ Чёрный  ⬜ Серый')
-        }
-       
-         console.log(arr);
+
+    if((arr[0]==="black" && (arr[1]==="white" ||arr[1]==="red") && arr[2]==="black"  && (arr[3]==="white" ||arr[3]==="red")&& arr[4]==="black")||
+    ((arr[0]==="white") && arr[1]==="black"  && (arr[2]==="white" ||arr[2]==="red")&& arr[3]==="black" && (arr[4]==="white" ||arr[4]==="red") ))
+    { 
+       ctx.reply( "Ставим:  ⬛ Чёрный  ⬜ Серый");
+       console.log("Ставим:  ⬛ Чёрный  ⬜ Серый");
+    }
+    if((arr[0]==="red" && (arr[1]==="white" ||arr[1]==="black") && arr[2]==="red"  && (arr[3]==="white" ||arr[3]==="black")&& arr[4]==="red"  )
+    ||((arr[0]==="white") && arr[1]==="red"  && (arr[2]==="white" ||arr[2]==="black")&& arr[3]==="red" && (arr[4]==="white" ||arr[4]==="black")))
+    {
+       ctx.reply( "Ставим: 🟥 Красный ⬜ Серый");
+       console.log("Ставим: 🟥 Красный ⬜ Серый");
+    }
+if((arr[0]==="black" && (arr[1]==="white" ||arr[1]==="grey") && arr[2]==="black"  && (arr[3]==="white" ||arr[3]==="grey")&& arr[4]==="black" )||
+((arr[0]==="white") && arr[1]==="black"  && (arr[2]==="white" ||arr[2]==="grey")&& arr[3]==="black" && (arr[4]==="white" ||arr[4]==="grey")))
+    {
+
+       ctx.reply( "Ставим:  ⬛ Чёрный 🟥 Красный");
+       console.log("Ставим:  ⬛ Чёрный 🟥 Красный");
+    }
+    
+if((arr[0]==="red" && (arr[1]==="white" ||arr[1]==="grey") && arr[2]==="red"  && (arr[3]==="white" ||arr[3]==="grey")&& arr[4]==="red" )||
+( (arr[0]==="white") && arr[1]==="red"  && (arr[2]==="white" ||arr[2]==="grey")&& arr[3]==="red" && (arr[4]==="white" ||arr[4]==="grey") ))
+    {
+       ctx.reply("Ставим: 🟥 Красный ⬛ Чёрный");
+       console.log("Ставим: 🟥 Красный ⬛ Чёрный");
+    }
+   
+    if((arr[0]==="grey" && (arr[1]==="white" ||arr[1]==="black") && arr[2]==="grey"  && (arr[3]==="white" ||arr[3]==="black")&& arr[4]==="grey" )||
+    ((arr[0]==="white") && arr[1]==="grey"  && (arr[2]==="white" ||arr[2]==="black")&& arr[3]==="grey" &&(arr[4]==="white" ||arr[4]==="black") ))
+    {   
+       ctx.reply("Ставим: ⬜ Серый 🟥 Красный ");
+       console.log("Ставим: ⬜ Серый 🟥 Красный");
+    }
+        if((arr[0]==="grey" && (arr[1]==="white" ||arr[1]==="red") && arr[2]==="grey"  && (arr[3]==="white" ||arr[3]==="red")&& arr[4]==="grey" )||
+        ( (arr[0]==="white" ) && arr[1]==="grey"  && (arr[2]==="white" ||arr[2]==="red")&& arr[3]==="grey" &&(arr[4]==="white" ||arr[4]==="red")))
+    {    
+       ctx.reply("Ставим:  ⬜Серый ⬛ Чёрный");
+       console.log("Ставим:  ⬜Серый ⬛ Чёрный");
+    }
+      
     }
     function good(){
         ctx.reply('Бот запущен!')
-       global.time= setInterval(request2,6000) 
+       global.time= setInterval(request2,60000) 
        }
 
 
 
 good()}
 )
+bot.hears('/end', async (ctx) => {
+    clearInterval(time);
+    ctx.reply("Пока");  
+})
 bot.launch()
 
